5093f04d46d9c2086837b0f09f4b46e555c54cd6
JanGaud <JanisGaudreault.jg@gmail.com>
2024-01-25 13:40:31 -0500

    Initialisation du d/but de portfolio 3D avec light et dark theme intègré

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,8 +6,17 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
+      colors: {
+        "raisin-black": "#211a1e",
+        "raisin-white": "#E5DEE2",
+        "persian-red": "#C3423F",
+        "yellow-green": "#9BC53D",
+        maize: "#FDE74C",
+        aero: "#5BC0EB",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
